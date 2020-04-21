26e26a8004563ec065fe6f33f645d59e5907b687
linglong <7467246+luckilycym@user.noreply.gitee.com>
2020-04-21 23:14:27 +0800

    第一次

---
+++ 算法集训2DAY6找零问题/背包问题.js
@@ -0,0 +1,35 @@
+/**
+ * 0-1 背包问题
+ * 告诉我背包的容量（可容纳东西的总重量）
+ * 同时告诉我，有哪些物品
+ * 每个物品有两个属性， 重量、价值
+ * 每个物品只有一个
+ * 问：如何装，才能得到最大的价值
+ * 返回的是一个数组，数组中记录了装配的结果
+ * @param {*} total
+ * @param {*} objects
+ */
+function package(total, objects) {}
+
+package(100, [
+  {
+    weight: 10,
+    price: 30
+  },
+  {
+    weight: 35,
+    price: 66
+  },
+  {
+    weight: 45,
+    price: 100
+  },
+  {
+    weight: 55,
+    price: 150
+  },
+  {
+    weight: 70,
+    price: 230
+  }
+]);
